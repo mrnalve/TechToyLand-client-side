869e917e6c95ffd5b7d5002070b2be86a2559658
Md Alve <mdalve1091@gmail.com>
2023-05-18 10:09:30 +0600

    design complete in registration page

--- src/Login/Register.jsx
+++ src/Login/Register.jsx
@@ -1,36 +1,36 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [photoURL, setPhotoURL] = useState('');
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const handleRegistration = (e) => {
     e.preventDefault();
-
-    let errorMessage = '';
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const photoURL = form.photo.value;
+    let errorMessage = "";
 
     if (!name) {
-      errorMessage = 'Please enter your name.';
+      errorMessage = "Please enter your name.";
     } else if (!email) {
-      errorMessage = 'Please enter your email.';
+      errorMessage = "Please enter your email.";
     } else if (!password) {
-      errorMessage = 'Please enter your password.';
+      errorMessage = "Please enter your password.";
     } else {
       // Email validation
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       if (!emailRegex.test(email)) {
-        errorMessage = 'Please enter a valid email address.';
+        errorMessage = "Please enter a valid email address.";
       }
 
       // Password validation
       const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/;
       if (!passwordRegex.test(password)) {
         errorMessage =
-          'Password should be at least 6 characters long and include at least one uppercase letter, one lowercase letter, and one number.';
+          "Password should be at least 6 characters long and include at least one uppercase letter, one lowercase letter, and one number.";
       }
     }
 
@@ -48,54 +48,62 @@ const Register = () => {
         {error && <p className="text-red-500 mb-4">{error}</p>}
         <form onSubmit={handleRegistration}>
           <div className="mb-4">
-            <label htmlFor="name" className="block text-gray-700 font-medium mb-2">
+            <label
+              htmlFor="name"
+              className="block text-gray-700 font-medium mb-2"
+            >
               Name
             </label>
             <input
               type="text"
               id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+              name="name"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-[#ffc837]"
               placeholder="Enter your name"
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 font-medium mb-2">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-medium mb-2"
+            >
               Email
             </label>
             <input
               type="email"
               id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+              name="email"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-[#ffc837]"
               placeholder="Enter your email"
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-700 font-medium mb-2">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-medium mb-2"
+            >
               Password
             </label>
             <input
               type="password"
               id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+              name="password"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-[#ffc837]"
               placeholder="Enter your password"
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="photoURL" className="block text-gray-700 font-medium mb-2">
+            <label
+              htmlFor="photoURL"
+              className="block text-gray-700 font-medium mb-2"
+            >
               Photo URL
             </label>
             <input
               type="text"
               id="photoURL"
-              value={photoURL}
-              onChange={(e) => setPhotoURL(e.target.value)}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
+              name="photo"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-[#ffc837]"
               placeholder="Enter your photo URL"
             />
           </div>
@@ -108,11 +116,11 @@ const Register = () => {
               Register
             </button>
             <p className="text-center text-gray-500 text-sm">
-          Already have an account?{" "}
-          <Link to="/login" className="text-[#ff8008] hover:underline">
-            Login here
-          </Link>
-        </p>
+              Already have an account?{" "}
+              <Link to="/login" className="text-[#ff8008] hover:underline">
+                Login here
+              </Link>
+            </p>
           </div>
         </form>
       </div>
@@ -121,4 +129,3 @@ const Register = () => {
 };
 
 export default Register;
-
