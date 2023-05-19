a0169918e36200b059c3f8cbed93f882652dbb3e
Md Alve <mdalve1091@gmail.com>
2023-05-19 16:45:47 +0600

    implement search button

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import ToysRow from "./ToysRow";
 
 const AllToys = () => {
   const toysData = useLoaderData();
+  const [allToysData, setAllToysData] = useState(toysData);
+  const [searchText, setSearchText] = useState("");
+  const handleSearch = () => {
+      fetch(`http://localhost:5000/getToysByText/${searchText}`)
+      .then(res => res.json())
+      .then(data => {
+        setAllToysData(data)
+        console.log(data);
+      })
+  };
   return (
     <div className="overflow-x-auto my-8">
+      <div className="flex justify-center my-4">
+        <input
+          type="text"
+          placeholder="Search for toys"
+          className="w-64 py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          onChange={(event) => setSearchText(event.target.value)}
+        />
+        <button onClick={handleSearch} className="bg-[#ff8008] hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-yellow-500">
+        Search
+      </button>
+      </div>
       <table className="table table-zebra w-full">
         {/* head */}
         <thead>
@@ -20,7 +41,7 @@ const AllToys = () => {
         </thead>
         <tbody>
           {/* row 1 */}
-          {toysData?.map((toyData) => (
+          {allToysData?.map((toyData) => (
             <ToysRow key={toyData._id} toyData={toyData}></ToysRow>
           ))}
         </tbody>
