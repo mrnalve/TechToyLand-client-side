85ae050a5ce9ea37444ee368593a43236e138ed5
Md Alve <mdalve1091@gmail.com>
2023-05-19 15:02:41 +0600

    added toys card in category tab

---
+++ src/Home/ShopByCategory/ShopByCategory.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import ToyCard from "./ToyCard";
+
+const ShopByCategory = () => {
+  const [activeTab, setActiveTab] = useState("ElectroniXplorers")
+  const [toys, setToys] = useState([])
+  const handleTabChange = tabName => {
+    setActiveTab(tabName)
+  }
+  useEffect(()=>{
+    fetch(`http://localhost:5000/toysByCategory/${activeTab}`)
+    .then(res=>res.json())
+    .then(data => {
+      console.log(data);
+      setToys(data)
+    })
+  },[activeTab])
+  return (
+    <div className="py-8 bg-gray-100 rounded-xl">
+      <div className="container mx-auto px-4">
+        <div>
+          <div className="flex justify-center space-x-4">
+            <button onClick={()=> handleTabChange("ElectroniXplorers")} className={`py-2 px-4 bg-white text-black border-none cursor-pointer rounded-xl hover:bg-gray-300 focus:bg-[#ffc837] focus:rounded-xl ${activeTab === "ElectroniXplorers" ? 'bg-[#ffc837] rounded-xl' : ''}`}>
+            ElectroniXplorers
+            </button>
+            <button onClick={()=> handleTabChange("Drone")} className={`py-2 px-4 bg-white text-black border-none cursor-pointer rounded-xl hover:bg-gray-300 focus:bg-[#ffc837] focus:rounded-xl ${activeTab === "Drone" ? 'bg-[#ffc837] rounded-xl' : ''}`}>
+            Drone
+            </button>
+            <button onClick={()=> handleTabChange("Camera")} className={`py-2 px-4 bg-white text-black border-none cursor-pointer rounded-xl hover:bg-gray-300 focus:bg-[#ffc837] focus:rounded-xl ${activeTab === "Camera" ? 'bg-[#ffc837] rounded-xl' : ''}`}>
+            Camera
+            </button>
+          </div>
+
+          <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
+             {
+              toys.map(toy => <ToyCard toy={toy} key={toy._id}></ToyCard>)
+             }
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopByCategory;
---
+++ src/Home/ShopByCategory/ToyCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ToyCard = ({ toy }) => {
+  const { pictureUrl, productName, price, rating } = toy;
+
+  return (
+    <div className="bg-white rounded-lg shadow-md p-4">
+      <img src={pictureUrl} alt={productName} className="w-full h-48 object-cover mb-4 rounded-lg" />
+      <h3 className="text-lg font-semibold">{productName}</h3>
+      <p className="text-gray-600 mt-2">${price}</p>
+      <div className="flex items-center mt-2">
+        <span className="text-yellow-500">{rating}</span>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-yellow-500 ml-1" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M10 0l6 18H4l6-18zm0 0l-6 18h12l-6-18zm-1.239 8.048l-.588-1.813L5 7.397l1.94-1.412L8.333 3l.555 1.985L10 7l1.112-1.015L13.06 3l1.392 2.985L15 7.397l-2.173.838-.588 1.813L10 8.476zM10 15.313l-3.531 2.156 1.032-3.563L3.75 9.876l3.625-.282L10 6l1.625 3.594 3.625.282-2.75 2.031 1.032 3.563L10 15.312z" clipRule="evenodd" />
+        </svg>
+      </div>
+      <button className="bg-[#ffc837] text-white mt-4 px-4 py-2 rounded-md hover:bg-[#ffb700]">
+        View Details
+      </button>
+    </div>
+  );
+};
+
+export default ToyCard;
